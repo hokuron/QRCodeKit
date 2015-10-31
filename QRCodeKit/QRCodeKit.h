dc0a22296a7027d427f88adca62a12533ec63996
shimizu <anti.soft.b@gmail.com>
2015-10-31 16:22:03 +0900

    Initial Commit

---
+++ QRCodeKit/QRCodeKit.h
@@ -0,0 +1,19 @@
+//
+//  QRCodeKit.h
+//  QRCodeKit
+//
+//  Created by hokuron on 2015/10/31.
+//  Copyright © 2015年 Takuma Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRCodeKit.
+FOUNDATION_EXPORT double QRCodeKitVersionNumber;
+
+//! Project version string for QRCodeKit.
+FOUNDATION_EXPORT const unsigned char QRCodeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCodeKit/PublicHeader.h>
+
+
